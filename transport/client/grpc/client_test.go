aaadc86b0233bdae6e5f85808922b78b3672de85
yates-z <yateszhou22@gmail.com>
2024-06-20 21:14:23 +0800

    feature: add timeout interceptor

---
+++ transport/client/grpc/client_test.go
@@ -0,0 +1,29 @@
+package grpc
+
+import (
+	"context"
+	"fmt"
+	"github.com/yates-z/easel/api"
+	"github.com/yates-z/easel/transport/client/grpc/interceptor/timeout"
+	"testing"
+	"time"
+)
+
+func TestNewClient(t *testing.T) {
+	client, err := NewClient(
+		WithInsecure(),
+		WithTarget("127.0.0.1:9100"),
+		UnaryInterceptor(timeout.UnaryClientInterceptor(time.Second)),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	c := api.NewGreeterClient(client)
+	res, err := c.SayHello(context.Background(), &api.HelloRequest{Name: ""})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(res)
+}
---
+++ transport/client/grpc/interceptor/timeout/timeout.go
@@ -0,0 +1,16 @@
+package timeout
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"time"
+)
+
+// UnaryClientInterceptor returns a new unary client interceptor that sets a timeout on the request context.
+func UnaryClientInterceptor(timeout time.Duration) grpc.UnaryClientInterceptor {
+	return func(ctx context.Context, method string, req, reply any, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		timedCtx, cancel := context.WithTimeout(ctx, timeout)
+		defer cancel()
+		return invoker(timedCtx, method, req, reply, cc, opts...)
+	}
+}
